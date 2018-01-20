44c540f2689a92cd50704b49a40979e0ebc19a80
madoos <maurice.ronet.dominguez@gmail.com>
2018-01-20 22:32:25 +0100

    Build: Refactorize test script

--- bin/run.sh
+++ bin/run.sh
@@ -8,7 +8,6 @@ docker run \
 -e USER=red-panda-ci \
 -e REPO=generator-redpanda-node-module \
 -e BRANCH=develop \
--e GITHUB_TOKEN="CHANGE" \
 -e NODE=6 \
 -e NPM_COMMAND=test \
 -e GENERATED_FILES=coverage \
--- bin/test.sh
+++ bin/test.sh
@@ -9,6 +9,5 @@ docker run \
 -e USER=madoos \
 -e REPO=type \
 -e BRANCH=develop \
--e NODE=6 \
 -e NPM_COMMAND=test \
 redpandaci/npm-command-runner:$VERSION
